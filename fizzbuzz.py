bb4dbca42ac1d02769c249907662e6f2e0249c3e
Chris Coe <ts24ccoe@gmail.com>
2014-08-05 15:41:32 -0400

    added commenting

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,8 @@
 def fizzbuzz():
+    '''
+    Prints fizz if divisble by 3, buzz if divisble by 5 and (only)
+    fizzbuzz if divisble by 15.  Also gives the number when anything is printed.
+    '''
     for num in range (1, 100):
         if num % 15 == 0:
             print('{}-->fizzbuzz'.format(num))
